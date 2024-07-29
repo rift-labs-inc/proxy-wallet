49ab221a805598274f0cb5c750a972004282d793
Cliff Syner <c4syner@gmail.com>
2024-07-28 19:02:52 -0500

    boilerplate

---
+++ popup.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react"
+import riftImage from "data-base64:~assets/rift.svg"
+import "./styles.css"
+
+function IndexPopup() {
+  const [data, setData] = useState("")
+  return (
+    <div
+      style={{
+        backgroundColor: "black",
+        width: "300px",  // Set your desired width
+        height: "400px", // Set your desired height
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center"
+      }}>
+      <img src={riftImage} alt="Rift" width={'84px'} height={'20px'}/>
+    </div>
+  )
+}
+
+export default IndexPopup;
